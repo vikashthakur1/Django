7e46418f60fd714b99b053deec1765d6621daace
Vikash <vikastsr@gmail.com>
2025-04-28 16:12:01 +0530

    2nd commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("welcome to first project")
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
 
 # Create your views here.
